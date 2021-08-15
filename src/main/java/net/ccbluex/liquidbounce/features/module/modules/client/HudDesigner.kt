0d0c0a1167695bfc4406a5e535e4c54a4a747c24
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2021-08-15 05:37:06 +0100

    Revert changes

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/HudDesigner.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/HudDesigner.kt
@@ -3,10 +3,10 @@ package net.ccbluex.liquidbounce.features.module.modules.client
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
-import net.ccbluex.liquidbounce.ui.client.hud.designer.GuiHUDDesigner
+import net.ccbluex.liquidbounce.ui.client.hud.designer.GuiHudDesigner
 
-@ModuleInfo(name = "HUDDesigner", category = ModuleCategory.CLIENT, canEnable = false)
-class HUDDesigner : Module() {
+@ModuleInfo(name = "HudDesigner", category = ModuleCategory.CLIENT, canEnable = false)
+class HudDesigner : Module() {
     override fun onEnable(){
         mc.displayGuiScreen(GuiHudDesigner())
     }
