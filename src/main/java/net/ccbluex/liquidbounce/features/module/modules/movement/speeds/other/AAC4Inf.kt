235dfe8ad24feed77c9ab73cbd84bd7f196909d8
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2021-08-15 02:54:54 +0100

    better

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/AAC4Inf.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/AAC4Inf.kt
@@ -9,17 +9,14 @@ class AAC4Inf : SpeedMode("AAC4Inf") {
             return;
         if (mc.thePlayer.onGround) {
             mc.thePlayer.jump();
-            mc.thePlayer.speedInAir = 0.02F;
-            mc.timer.timerSpeed = 1.00F;
+            mc.timer.timerSpeed = 0.99F;
         }
         if (mc.thePlayer.fallDistance > 0.7 && mc.thePlayer.fallDistance < 1.3) {
-            mc.thePlayer.speedInAir = 0.02F;
-            mc.timer.timerSpeed = 1.08F;
+            mc.timer.timerSpeed = 1.04F;
         }
     }
 
     override fun onDisable() {
-        mc.thePlayer!!.speedInAir = 0.02f
         mc.timer.timerSpeed = 1f
     }
 }
