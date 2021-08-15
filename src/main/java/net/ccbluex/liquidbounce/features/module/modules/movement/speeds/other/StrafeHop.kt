1236e6be12df9f93a0e5011363e7495b58538e4e
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2021-08-15 03:54:09 +0100

    Update StrafeHop.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/StrafeHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/StrafeHop.kt
@@ -9,7 +9,7 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMod
 import net.ccbluex.liquidbounce.utils.MovementUtils
 
 class StrafeHop : SpeedMode("StrafeHop") {
-    override fun preMotion() {
+    override fun onPreMotion() {
         if (mc.thePlayer.isInWater) return
         if (MovementUtils.isMoving()) {
             if (mc.thePlayer.onGround) mc.thePlayer.jump() else MovementUtils.strafe(MovementUtils.getSpeed() * 1f)
